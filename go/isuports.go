9ec02b9baa295b32efeced30a5b55a9e63279278
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-07-23 15:29:47 +0900

    これ動くのかなぁ

--- go/isuports.go
+++ go/isuports.go
@@ -1302,30 +1302,23 @@ func playerHandler(c echo.Context) error {
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
 	fl, err := flockByTenantID(v.tenantID)
 	if err != nil {
+		fl.Close()
 		return fmt.Errorf("error flockByTenantID: %w", err)
 	}
-	defer fl.Close()
 	pss := make([]PlayerScoreRow, 0, len(cs))
-	for _, c := range cs {
-		ps := PlayerScoreRow{}
-		if err := tenantDB.GetContext(
-			ctx,
-			&ps,
-			// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
-			"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? AND player_id = ? ORDER BY row_num DESC LIMIT 1",
-			v.tenantID,
-			c.ID,
-			p.ID,
-		); err != nil {
-			// 行がない = スコアが記録されてない
-			if errors.Is(err, sql.ErrNoRows) {
-				continue
-			}
-			return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, c.ID, p.ID, err)
-		}
-		pss = append(pss, ps)
+	query := `
+		SELECT * FROM player_score AS s WHERE tenant_id = ? AND player_id = ? JOIN 
+		(SELECT MAX(row_num) AS num, competition_id FROM player_score WHERE tenant_id = ? AND player_id = ? GROUP BY competition_id) AS t 
+		ON s.competition_id = t.competition_id AND s.row_num = t.num
+	`
+	err = tenantDB.SelectContext(ctx, &pss, query, v.tenantID, p.ID, v.tenantID, p.ID)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		fl.Close()
+		return fmt.Errorf("error Select player_score: %w", err)
 	}
+	fl.Close()
 
+	// N+1
 	psds := make([]PlayerScoreDetail, 0, len(pss))
 	for _, ps := range pss {
 		comp, err := retrieveCompetition(ctx, tenantDB, ps.CompetitionID)
