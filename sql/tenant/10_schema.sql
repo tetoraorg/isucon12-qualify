71d561d7ed0f17de2e488c4cd12e6c9aad5e83c2
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-07-23 13:58:27 +0900

    sqliteでindexを作成

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -28,5 +28,6 @@ CREATE TABLE player_score (
   score BIGINT NOT NULL,
   row_num BIGINT NOT NULL,
   created_at BIGINT NOT NULL,
-  updated_at BIGINT NOT NULL
+  updated_at BIGINT NOT NULL,
+  INDEX idx_tenant_competition_row (tenant_id, competition_id, row_num)
 );
