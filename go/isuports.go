fa09e8b6630b7d9dae536e691e9ae3af0f5f4c70
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-07-23 16:06:09 +0900

    雑にコネクション数増やした

--- go/isuports.go
+++ go/isuports.go
@@ -205,7 +205,8 @@ func Run() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	adminDB.SetMaxOpenConns(10)
+	adminDB.SetMaxOpenConns(256)
+	adminDB.SetMaxIdleConns(64)
 	defer adminDB.Close()
 
 	port := getEnv("SERVER_APP_PORT", "3000")
